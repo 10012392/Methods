32ef523fb6570ec04c7c170b92fd14c83d5fa88c
Lara Corkery <10012392@student.toiohomai.ac.nz>
2019-05-02 13:48:00 +1200

    Method Exercises

---
+++ MethodExercise2/MethodExercise2/Program.cs
@@ -0,0 +1,37 @@
+﻿//Lara Corkery 02/05/2019
+//Completing Method Exercise 2
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodExercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exercise Two 
+            Console.WriteLine("This Application Will Convert Miles into Kilometers:");
+            Console.WriteLine("Enter The Amount Of Miles:");
+            int miles1 = int.Parse(Console.ReadLine());
+            double total = Conversion(miles1);
+
+            Console.WriteLine("The Amount In Miles Is: " + miles1);
+            Console.WriteLine("The Result In Kilometers Is: " + total);
+            Console.ReadLine();
+           
+        }
+        static double Conversion(int miles1)
+        {
+            return miles1 * 1.609 ;
+        }
+
+
+
+
+
+    
+    }
+}
---
+++ MethodExercises/MethodExercises/Program.cs
@@ -0,0 +1,34 @@
+﻿//Lara Corkery 02/05/2019
+//Completing Method Exercise 1
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exercise One 
+            Console.WriteLine("This Application Will Do Something ");
+            Console.WriteLine("Please Enter A Number: ");
+            int num1 = int.Parse(Console.ReadLine());
+            int total = ReturnNumbers(num1);
+
+            Console.WriteLine(total);
+            Console.ReadLine();
+        }
+            static int ReturnNumbers(int num1)
+            {
+                return num1 - num1 * 2;
+            }
+
+
+
+           
+     
+    }
+}
