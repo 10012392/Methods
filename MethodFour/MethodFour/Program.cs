5393f361aa3d506e51732129bf3f770d94b123a9
Lara Corkery <10012392@student.toiohomai.ac.nz>
2019-05-08 10:07:52 +1200

    Method Exercises Completed

---
+++ MethodFour/MethodFour/Program.cs
@@ -0,0 +1,26 @@
+﻿//Lara Corkery 08/05/2019
+//Exercise Four 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodFour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please Enter A Number From 1 - 7: ");
+            Console.WriteLine("\nThat Number Corresponds To: " + Day(int.Parse(Console.ReadLine())));
+            Console.ReadLine();
+        }
+
+        static string Day(int num)
+        {
+            string[] days = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+            return days[num - 1];
+        }
+    }
+}
---
+++ MethodThree/MethodThree/Program.cs
@@ -0,0 +1,32 @@
+﻿//Lara Corkery 08/05/2019
+//Exercise Three 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodThree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter The Radius Of The Circle: ");
+            double input = double.Parse(Console.ReadLine());
+            Console.WriteLine("The Circumference Of The Circle With A Radius Of " + input + " Is " + Math.Round(Circumference(input)));
+            Console.WriteLine("The Area of A Circle With A Radius Of " + input + " Is " + Math.Round(Area(input)));
+            Console.ReadLine();
+        }
+
+        static double Circumference(double rad)
+        {
+            return rad * 2 * Math.PI;
+        }
+
+        static double Area(double rad)
+        {
+            return (rad * rad) * Math.PI;
+        }
+    }
+}
